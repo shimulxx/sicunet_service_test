46d169f5ed2270a45bc2a6aed9e72969e1e20d56
sicunetappdeveloper <shimul6680@gmail.com>
2024-11-05 16:35:21 +0600

    first

--- app/src/main/java/com/example/sicunetservicetest/MainActivity.kt
+++ app/src/main/java/com/example/sicunetservicetest/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.sicunetservicetest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.sicunetservicetest.databinding.ActivityMainBinding
+import android.Manifest
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     private var value = 0
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,6 +26,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.buttonStartBle.setOnClickListener {
             Log.d(tag, "onCreate: clicked ${++value}")
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1)
         }
         val serviceIntent = Intent(this, MyForegroundService::class.java)
         ContextCompat.startForegroundService(this, serviceIntent)

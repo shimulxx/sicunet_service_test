08073ed95a0b6a38a9d86431955b96ba4d782b84
sicunetappdeveloper <shimul6680@gmail.com>
2025-02-13 17:10:46 +0600

    ssid work

--- app/src/main/java/com/example/sicunetservicetest/MainActivity.kt
+++ app/src/main/java/com/example/sicunetservicetest/MainActivity.kt
@@ -1,182 +1,83 @@
 package com.example.sicunetservicetest
-import android.app.admin.DevicePolicyManager
-import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
+import android.net.wifi.ScanResult
+import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
-import android.os.PowerManager
-import android.provider.Settings
 import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.sicunetservicetest.databinding.ActivityMainBinding
-import com.hwit.HwitManager.HwitGetBoardEthIp
-import com.hwit.HwitManager
-import com.hwit.HwitManager.HwitGetCpuTemp
-import com.hwit.HwitManager.HwitSetIOValue
-import com.hwit.HwitManager.HwitGetIOValue
-import com.hwit.HwitManager.HwitRebootSystem
-import com.hwit.HwitManager.getAvailableCpuFreq
-import com.hwit.HwitManager.HwitSetWifiDhcpIpConnect
-import com.hwit.HwitManager.HwitSetWifiStaticIpConnect
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var wifiManager: WifiManager
+
     private var tag = "MainActivity"
 
     private var value = 0
 
-    val PERMISSION_REQUEST_CODE: Int = 1
+    private val MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 100
 
+    private fun checkWifiPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        } else {
+            // Permissions already granted, proceed with Wi-Fi scanning
+            scanWifiNetworks()
+        }
+    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String?>,
         grantResults: IntArray
     ) {
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, proceed with accessing phone state
-            } else {
-                // Permission denied, inform the user
+        if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                scanWifiNetworks()
+            }
+            else {
+                Toast.makeText(this, "Location permission is required to scan Wi-Fi networks", Toast.LENGTH_SHORT).show()
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun scanWifiNetworks() {
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        val scanResults: List<ScanResult> = wifiManager.scanResults
+
+        for (scanResult in scanResults) {
+            val ssid = scanResult.SSID
+            Log.d("SSID", ssid)
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        // Set the content view to the root of the binding object
         setContentView(binding.root)
-        val currentValue = HwitGetIOValue(5)
-        Log.d(tag, "onCreate: current value: $currentValue")
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1)
-//        }
-//        val serviceIntent = Intent(this, MyForegroundService::class.java)
         binding.buttonStartBle.setOnClickListener {
-//            Log.d(tag, "onCreate: clicked ${++value}")
-//            val powerManager = getSystemService(POWER_SERVICE) as PowerManager?
-//            powerManager?.reboot(null)
-            //HwitSetIOValue(5, 1)
-            //HwitRebootSystem(this)
-            //ContextCompat.startForegroundService(this, serviceIntent)
 
-            HwitSetWifiStaticIpConnect(
-                this,
-                "Sicunet 5G",
-                "sicunet2025",
-                2,
-                "192.168.1.200",
-                "192.168.1.1",
-                "255.255.255.0",
-                "8.8.8.8",
-                "8.8.4.4"
-            )
         }
         binding.buttonStopService.setOnClickListener {
-//            Log.d(tag, "onCreate: clicked ${++value}")
-//            applicationContext.stopService(serviceIntent)
-            //val result = HwitGetCpuTemp()
-            //HwitSetIOValue(5, 0)
-//            val result = HwitGetBoardEthIp(this)
-            //Log.d(tag, "onCreate: stopped ${result}")
 
-            //stopLockTask()
-
-            HwitSetWifiDhcpIpConnect(
-                this,
-                "Sicunet 5G",
-                "sicunet2025",
-                2,
-            )
         }
-//        binding.timerService.text = "${MyForegroundService.tickValue}"
-//        requestPermission()
-//
-//        //before run lock screen mode write the following command on adb is mandatory
-//        //adb shell dpm set-device-owner com.example.sicunetservicetest/.MyDeviceAdminReceiver
-//
-//        startLockTaskMode()
-
-        //to remove ownership following ownership command is needed
-//        adb shell dpm remove-active-admin com.example.sicunetservicetest/.MyDeviceAdminReceiver
-//        adb shell pm clear com.example.sicunetservicetest
-//        adb shell pm uninstall com.example.sicunetservicetest
-
-        //pinScreen()
-
-        //requestPermission2()
+        checkWifiPermission()
     }
 
-    private fun requestPermission2(){
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.REBOOT)
-            != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.REBOOT),
-                PERMISSION_REQUEST_CODE
-            );
-        }
-    }
-
-    private fun startLockTaskMode() {
-        try {
-            if (isDeviceOwner()) {
-                startLockTask() // Locks the screen with this activity
-            } else {
-                Log.d("MyMessage", "App is not the Device Owner. Lock Task mode unavailable.")
-                // Provide fallback or notify the user
-                //println("App is not the Device Owner. Lock Task mode unavailable.")
-            }
-        }
-        catch (e: Exception){
-            Log.d("MyMessage", "${e.message}: ")
-        }
-
-    }
-
-    private fun isDeviceOwner(): Boolean {
-        val dpm = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
-        val adminComponent = ComponentName(this, MyDeviceAdminReceiver::class.java)
-        return dpm.isDeviceOwnerApp(packageName) && dpm.isAdminActive(adminComponent)
-    }
-
-//    private fun pinScreen() {
-//        val flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-//                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-//                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-//                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-//                View.SYSTEM_UI_FLAG_FULLSCREEN or
-//                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//        window.decorView.systemUiVisibility = flags
-//    }
-
-    private fun requestPermission() {
-        val overlayPermissionLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            // Check the result of the permission request
-            if (Settings.canDrawOverlays(this)) {
-                //onOverlayPermissionGranted()
-            } else {
-                //onOverlayPermissionDenied()
-            }
-        }
-        if (!Settings.canDrawOverlays(this)) {
-            val intent = Intent(
-                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:" + this.packageName)
-            )
-            overlayPermissionLauncher.launch(intent)
-            //startActivityForResult(intent, 232)
-        } else {
-            //Permission Granted-System will work
-        }
-    }
 }
